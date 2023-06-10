814e609d85fe10f73a68ef2602f092c44c1ef482
GranDSenpai <130882535+GranDSenpai@users.noreply.github.com>
2023-06-10 10:07:56 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,756 @@
+const maps = [
+    {
+      name: "Andromeda made isshun de / IA & Hatsune Miku",
+      type: "Aim",
+      artist: "Omoi",
+      difficulty: 7.18,
+      date: "16-11-2016",
+      bpm: 195,
+      link: "https://osu.ppy.sh/beatmapsets/533925#osu/1131089"
+    },
+    {
+      name: "Clarity feat. Foxes (IOSYS uno DENPA Remix)",
+      type: "Aim",
+      artist: "Zedd",
+      difficulty: 7.54,
+      date: "20-05-2017",
+      bpm: 200,
+      link: "https://osu.ppy.sh/beatmapsets/615644#osu/1298530"
+    },
+    {
+     name: "Los! Los! Los!",
+     type: "Aim",
+     artist: "Tanya Degurechaff (CV: Aoi Yuuki)",
+     difficulty: 7.88,
+     date: "23-05-2017",
+     bpm: 240,
+     link: "https://osu.ppy.sh/beatmapsets/630437#osu/1330877"
+    },
+    {
+     name: "fantastic dreamer",
+     type: "Experimental Awkward",
+     artist: "Machico",
+     difficulty: 6.01,
+     date: "12-11-2018",
+     bpm: 185,
+     link: "https://osu.ppy.sh/beatmapsets/878414#osu/1837139"
+    },
+    {
+     name: "Sayonara Bystander",
+     type: "Experimental Awkward",
+     artist: "YUKI",
+     difficulty: 5.25 ,
+     date: "27-11-2018",
+     bpm: 163,
+     link: "https://osu.ppy.sh/beatmapsets/885081#osu/1849915"
+      },
+      {
+    name: "post-modern weeaboo",
+    type: "Experimental Wiggle",
+    artist: "goreshit",
+    difficulty: 8.18,
+    date: "08-08-2018",
+    bpm: 210,
+    link: "https://osu.ppy.sh/beatmapsets/828152#osu/1735214"
+    },
+    {
+    name: "Hanezeve Caradinha (ft. Takeshi Saito)",
+    type: "Flow",
+    artist: "Kevin Penkin",
+    difficulty: 7.22,
+    date: "02-05-2018",
+    bpm: 160,
+    link: "https://osu.ppy.sh/beatmapsets/774416#osu/1895746"
+    },
+    {
+    name: "The Girls Are Alright! ~TV size~",
+    type: "Aim",
+    artist: "Saya",
+    difficulty: 6.10,
+    date: "09-05-2019",
+    bpm: 168,
+    link: "https://osu.ppy.sh/beatmapsets/936951#osu/1957235"
+    },
+    {
+    name: "Responsibility Response",
+    type: "Aim",
+    artist: "Izumi Akazawa (CV Madoka Yonezawa)",
+    difficulty: 7.58,
+    date: "13-03-2019",
+    bpm: 202,
+    link: "https://osu.ppy.sh/beatmapsets/939380#osu/1962125"
+    },
+    {
+    name: "Connecting Happy!!",
+    type: "Aim",
+    artist: "Pekori-nu(CV:M.A.O) & Kokkoro(CV:Ito Miku) & Kyaru(CV:Tachibana Rika)",
+    difficulty: 6.94,
+    date: "24-03-2019",
+    bpm: 182,
+    link: "https://osu.ppy.sh/beatmapsets/945143#osu/1973706"
+    },
+    {
+    name: "Anoko ni Drop Kick",
+    type: "Aim",
+    artist: "Jashin Girls",
+    difficulty: 9.08,
+    date: "10-04-2019",
+    bpm: 230,
+    link: "https://osu.ppy.sh/beatmapsets/953584#osu/1991017"
+    },
+    {
+    name: "Machikado Tangent",
+    type: "Experimental Weird Alt",
+    artist: "Yoshida Yuuko (CV: Kohara Konomi), Chiyoda Momo (CV: Kito Akari)",
+    difficulty: 6.02,
+    date: "06-09-2019",
+    bpm: 165,
+    link: "https://osu.ppy.sh/beatmapsets/1033279#osu/2167668"
+    },
+    {
+    name: "Three Piece",
+    type: "Aim",
+    artist: "Hanako (CV: Hina Kino), Olivia (CV: Rika Nagae), Kasumi (CV: Konomi Kohara)",
+    difficulty: 5.82,
+    date: "18-04-2019",
+    bpm: 163,
+    link: "https://osu.ppy.sh/beatmapsets/957897#osu/2005627"
+    },
+    {
+    name: "Unreal Mokushi Sou",
+    type: "Shit",
+    artist: "Omoi",
+    difficulty: 7.96,
+    date: "07-12-2015",
+    bpm: 195,
+    link: "https://osu.ppy.sh/beatmapsets/387175#osu/1802189"
+    },
+    {
+    name: "Rakuen Toshi",
+    type: "Jazz",
+    artist: "Masayoshi Ooishi",
+    difficulty: 6.56,
+    date: "20-08-2019",
+    bpm: 276,
+    link: "https://osu.ppy.sh/beatmapsets/1024293#osu/2142571"
+    },
+    {
+    name: "Recall THE END (TV Size)",
+    type: "Aim",
+    artist: "TRUSTRICK",
+    difficulty: 6.75,
+    date: "29-01-2020",
+    bpm: 184,
+    link: "https://osu.ppy.sh/beatmapsets/1101201#osu/2300519"
+    },
+    {
+    name: "Sunflower",
+    type: "DT",
+    artist: "Post Malone, Swae Lee",
+    difficulty: 5.41,
+    date: "02-05-2020",
+    bpm: 90,
+    link: "https://osu.ppy.sh/beatmapsets/1160807#osu/2421869"
+    },
+    {
+    name: "Ai wa fushicho no yo ni",
+    type: "Aim",
+    artist: "Shinku no bara no kishi Prim",
+    difficulty: 7.58,
+    date: "18-05-2020",
+    bpm: 192,
+    link: "https://osu.ppy.sh/beatmapsets/1173437#osu/2447573"
+    },
+    {
+    name: "Megumi no Ame (TV Size)",
+    type: "DT",
+    artist: "yanaginagi",
+    difficulty: 5.70,
+    date: "15-05-2020",
+    bpm: 182,
+    link: "https://osu.ppy.sh/beatmapsets/1171198#osu/2443072"
+    },
+    {
+    name: "with you",
+    type: "Aim",
+    artist: "ClariS",
+    difficulty: 6.4,
+    date: "23-05-2020",
+    bpm: 160,
+    link: "https://osu.ppy.sh/beatmapsets/1176997#osu/2454738"
+    },
+    {
+    name: "Kamiiro Awase",
+    type: "DT",
+    artist: "binaria",
+    difficulty: 5.15,
+    date: "24-02-2019",
+    bpm: 150,
+    link: "https://osu.ppy.sh/beatmapsets/930863#osu/2381716"
+    },
+    {
+    name: "Rebellion",
+    type: "Aim",
+    artist: "3FORCE",
+    difficulty: 6.73,
+    date: "13-07-2020",
+    bpm: 160,
+    link: "https://osu.ppy.sh/beatmapsets/1212981#osu/2524628"
+    },
+    {
+    name: "LAST STARDUST",
+    type: "Experimental AR8 flow",
+    artist: "Aimer",
+    difficulty: 5.25,
+    date: "29-05-2020",
+    bpm: 81.33,
+    link: "https://osu.ppy.sh/beatmapsets/1180765#osu/2462032"
+    },
+    {
+    name: "Chocobo",
+    type: "Aim",
+    artist: "Hyadain",
+    difficulty: 7.03,
+    date: "11-08-2020",
+    bpm: 170,
+    link: "https://osu.ppy.sh/beatmapsets/1233869#osu/2564952"
+    },
+    {
+    name: "Starboy x Survive - Stranger Boy",
+    type: "Aim",
+    artist: "flipboitamidle5",
+    difficulty: 7.61,
+    date: "06-08-2020",
+    bpm: 93,
+    link: "https://osu.ppy.sh/beatmapsets/1230392#osu/2557938"
+    },
+        {
+    name: "StaRt",
+    type: "Aim",
+    artist: "Shirakami Fubuki",
+    difficulty: 7.8,
+    date: "25-08-2020",
+    bpm: 190,
+    link: "https://osu.ppy.sh/beatmapsets/1243235#osu/2584689"
+    },
+    {
+    name: "Un'altra Persona (Diavolo's Theme darksynth 80s remix) by Astrophysics",
+    type: "Aim",
+    artist: "Yugo Kanno",
+    difficulty: 7.4,
+    date: "08-09-2020",
+    bpm: 180,
+    link: "https://osu.ppy.sh/beatmapsets/1253583#osu/2605182"
+    },
+    {
+    name: "Piracy - It's a Crime",
+    type: "Aim",
+    artist: "MPAA",
+    difficulty: 7.9,
+    date: "19-12-2020",
+    bpm: 107.3,
+    link: "https://osu.ppy.sh/beatmapsets/1326770#osu/2748719"
+    },
+    {
+    name: "Yukitoki",
+    type: "DT",
+    artist: "yanaginagi",
+    difficulty: 5.3,
+    date: "14-06-2019",
+    bpm: 160,
+    link: "https://osu.ppy.sh/beatmapsets/1003048#osu/2182200"
+    },
+    {
+    name: "confusion",
+    type: "Aim",
+    artist: "nowisee",
+    difficulty: 7.08,
+    date: "25-01-2021",
+    bpm: 176,
+    link: "https://osu.ppy.sh/beatmapsets/1356376#osu/2807129"
+    },
+    {
+    name: "Envy Baby",
+    type: "Aim",
+    artist: "Omaru Polka",
+    difficulty: 6.67,
+    date: "23-02-2021",
+    bpm: 171,
+    link: "https://osu.ppy.sh/beatmapsets/1382813#osu/2856844"
+    },
+    {
+    name: "Go Berzerk",
+    type: "Aim (Tournament)",
+    artist: "Scott Brown",
+    difficulty: 6.47,
+    date: "7-04-2021",
+    bpm: 172,
+    link: "https://osu.ppy.sh/beatmapsets/1425212#osu/2934837"
+    },
+    {
+    name: "Recall THE END",
+    type: "Aim",
+    artist: "TRUSTRICK",
+    difficulty: 7.54,
+    date: "19-05-2021",
+    bpm: 184,
+    link: "https://osu.ppy.sh/beatmapsets/1466751#osu/3012187"
+    },
+    {
+    name: "OVER THE TOP (TV Size)",
+    type: "Aim",
+    artist: "Kitadani Hiroshi",
+    difficulty: 8.32,
+    date: "2-05-2021",
+    bpm: 212,
+    link: "https://osu.ppy.sh/beatmapsets/1449650#osu/2980529"
+    },
+    {
+    name: "Black Swallowtail",
+    type: "Aim",
+    artist: "UROBOROS",
+    difficulty: 8.04,
+    date: "29-05-2021",
+    bpm: 204,
+    link: "https://osu.ppy.sh/beatmapsets/1476118#osu/3028955"
+    },
+    {
+    name: "115",
+    type: "Consistency",
+    artist: "Treyarch Sound",
+    difficulty: 7.49,
+    date: "13-06-2021",
+    bpm: 200,
+    link: "https://osu.ppy.sh/beatmapsets/1491038#osu/3056420"
+    },
+    {
+    name: "Breaking The Habit",
+    type: "Aim (Tournament)",
+    artist: "Linkin Park",
+    difficulty: 6.54,
+    date: "09-08-2021",
+    bpm: 200.05,
+    link: "https://osu.ppy.sh/beatmapsets/1543673#osu/3155346"
+    },
+    {
+    name: "We Are Japanese Goblin",
+    type: "Aim (Tournament)",
+    artist: "IOSYS",
+    difficulty: 6.68,
+    date: "17-08-2021",
+    bpm: 200,
+    link: "https://osu.ppy.sh/beatmapsets/1551234#osu/3169947"
+    },
+    {
+    name: "Jet Rocket",
+    type: "Aim (Tournament)",
+    artist: "LiSa",
+    difficulty: 6.99,
+    date: "12-08-2021",
+    bpm: 178,
+    link: "https://osu.ppy.sh/beatmapsets/1547179#osu/3161838"
+    },
+    {
+    name: "Shonandai no Uta",
+    type: "Experimental AR8 flow",
+    artist: "Setetsu Record",
+    difficulty: 6.38,
+    date: "14-07-2021",
+    bpm: 89,
+    link: "https://osu.ppy.sh/beatmapsets/1519363#osu/3109944"
+    },
+    {
+    name: "The Piercing Moonlight",
+    type: "Aim",
+    artist: "Astrophysics",
+    difficulty: 8.04,
+    date: "05-07-2021",
+    bpm: 194,
+    link: "https://osu.ppy.sh/beatmapsets/1483005#osu/3041751"
+    },
+    {
+    name: "My Brother (Definitive Version)",
+    type: "Aim",
+    artist: "Shiro Sagisu",
+    difficulty: 9.39,
+    date: "13-09-2021",
+    bpm: 124,
+    link: "https://osu.ppy.sh/beatmapsets/1576000#osu/3217677"
+    },
+    {
+    name: "Manemane Psychotropic",
+    type: "Aim (Tournament)",
+    artist: "MARETU feat.",
+    difficulty: 7.36,
+    date: "24-08-2021",
+    bpm: 210,
+    link: "https://osu.ppy.sh/beatmapsets/1557924#osu/3182481"
+    },
+    {
+    name: "The Watchers",
+    type: "Aim",
+    artist: "3FORCE",
+    difficulty: 7.57,
+    date: "09-10-2021",
+    bpm: 180,
+    link: "https://osu.ppy.sh/beatmapsets/1597512#osu/3262549"
+    },
+    {
+    name: "Catch up! - Part 2",
+    type: "Aim",
+    artist: "SonicPanic",
+    difficulty: 7.33,
+    date: "13-10-2021",
+    bpm: 169.8,
+    link: "https://osu.ppy.sh/beatmapsets/1600656#osu/3268868"
+    },
+    {
+    name: "Quo Vadis",
+    type: "Aim (Tournament)",
+    artist: "M2U",
+    difficulty: 6.63,
+    date: "1-12-2021",
+    bpm: 160,
+    link: "https://osu.ppy.sh/beatmapsets/1640562#osu/3348480"
+    },
+    {
+    name: "Hisoka's Theme",
+    type: "Experimental Aim",
+    artist: "Yoshihisa Hirano",
+    difficulty: 7.83,
+    date: "24-10-2021",
+    bpm: 234,
+    link: "https://osu.ppy.sh/beatmapsets/1609755#osu/3286802"
+    },
+    {
+    name: "Descent",
+    type: "Stamina (Tournament)",
+    artist: "Feryquitous",
+    difficulty: 7.36,
+    date: "15-12-2021",
+    bpm: 220,
+    link: "https://osu.ppy.sh/beatmapsets/1650836#osu/3370053"
+    },
+    {
+    name: "She Moves Like A Knife",
+    type: "Aim",
+    artist: "Perturbator",
+    difficulty: 7.02,
+    date: "29-03-2021",
+    bpm: 155,
+    link: ""
+    },
+    {
+    name: "C18H27NO3(extend)",
+    type: "Consistency",
+    artist: "Team Grimoire",
+    difficulty: 8.09,
+    date: "09/01/2022",
+    bpm: 210,
+    link: "https://osu.ppy.sh/beatmapsets/1670195#osu/3411280"
+    },
+    {
+    name: "The Kill (Bury Me) (Edit)",
+    type: "Aim",
+    artist: "Thirty Seconds To Mars",
+    difficulty: 7.45,
+    date: "23-01-2022",
+    bpm: 183,
+    link: "https://osu.ppy.sh/beatmapsets/1680651#osu/3433537"
+    },
+    {
+    name: "Cruel Clocks",
+    type: "Awkward Aim (Tournament)",
+    artist: "himeringo",
+    difficulty: 7.11,
+    date: "27-03-2022",
+    bpm: 220,
+    link: "https://osu.ppy.sh/beatmapsets/1728939#osu/3533412"
+    },
+    {
+    name: "-ERROR",
+    type: "Aim (Tournament)",
+    artist: "Tokoyami Towa",
+    difficulty: 7.9,
+    date: "15-03-2022",
+    bpm: 200,
+    link: "https://osu.ppy.sh/beatmapsets/1719908#osu/3514870"
+    },
+    {
+    name: "Etude for the Sinners",
+    type: "Experimental Streams",
+    artist: "Ice",
+    difficulty: 8.17,
+    date: "21-12-2021",
+    bpm: 200,
+    link: "https://osu.ppy.sh/beatmapsets/1655436#osu/3379100"
+    },
+    {
+    name: "Mate Um Bonito Hoje Mesmo!",
+    type: "Aim",
+    artist: "Marcioz",
+    difficulty: 7.54,
+    date: "30-06-2022",
+    bpm: 207.45,
+    link: "https://osu.ppy.sh/beatmapsets/1796770#osu/3683880"
+    },
+    {
+    name: "Hachidaijigoku Jakontensei Kyousou Sanbika",
+    type: "Aim",
+    artist: "Sakamirito",
+    difficulty: 8.19,
+    date: "01-08-2022",
+    bpm: 203,
+    link: "https://osu.ppy.sh/beatmapsets/1819336#osu/3733062"
+    },
+    {
+    name: "Egao no Kanata",
+    type: "Aim",
+    artist: "Chiho feat. majiko",
+    difficulty: 7.54,
+    date: "03-03-2021",
+    bpm: 185,
+    link: "https://osu.ppy.sh/beatmapsets/1390875#osu/3683905"
+    },
+    {
+    name: "Unwelcome School",
+    type: "Aim",
+    artist: "Mitsukiyo",
+    difficulty: 7.72,
+    date: "10-07-2021",
+    bpm: 180,
+    link: "https://osu.ppy.sh/beatmapsets/1515686#osu/3103130"
+    },
+    {
+    name: "terminus//runtimes",
+    type: "Aim",
+    artist: "Katagiri",
+    difficulty: 8.08,
+    date: "10-08-2022",
+    bpm: 200,
+    link: "https://osu.ppy.sh/beatmapsets/1825729#osu/3746773"
+    },
+    {
+    name: "Iyana Wotsukai",
+    type: "Aim",
+    artist: "Yukiri Kotobuki",
+    difficulty: 7.91,
+    date: "11-08-2022",
+    bpm: 190,
+    link: "https://osu.ppy.sh/beatmapsets/1826371#osu/3748163"
+    },
+    {
+    name: "Hoshi to Koisuru Concerto",
+    type: "Aim",
+    artist: "nayuta",
+    difficulty: 6.98,
+    date: "26-07-2022",
+    bpm: 175,
+    link: "https://osu.ppy.sh/beatmapsets/1814794#osu/3722933"
+    },
+    {
+    name: "All Fool",
+    type: "Aim",
+    artist: "KANKAN feat. Hiiragi Kirai",
+    difficulty: 8.07,
+    date: "12-07-2022",
+    bpm: 195,
+    link: "https://osu.ppy.sh/beatmapsets/1814794#osu/3722933"
+    },
+    {
+    name: "Orbital strike",
+    type: "Aim",
+    artist: "Helblinde",
+    difficulty: 8.4,
+    date: "25-08-2022",
+    bpm: 195,
+    link: "https://osu.ppy.sh/beatmapsets/1835522#osu/3768437"
+    },
+    {
+    name: "Lilith ambivalence lovers",
+    type: "Experimental Flow",
+    artist: "ikaruga_bex",
+    difficulty: 7.38,
+    date: "31-08-2022",
+    bpm: 151,
+    link: "https://osu.ppy.sh/beatmapsets/1839435#osu/3777683"
+    },
+    {
+    name: "Sweet Rain",
+    type: "Experimental Flow",
+    artist: "Y&Co. feat. Karin",
+    difficulty: 6.93,
+    date: "28-08-2022",
+    bpm: 155,
+    link: "https://osu.ppy.sh/beatmapsets/1837519#osu/3773074"
+    },
+    {
+    name: "STONE OCEAN (TURBO Eurobeat Remix)",
+    type: "Aim (Tournament)",
+    artist: "ichigo from KISIDA KYODAN & THE AKEBOSI ROCKETS",
+    difficulty: 6.98,
+    date: "11-09-2022",
+    bpm: 180,
+    link: "https://osu.ppy.sh/beatmapsets/1846248#osu/3792648"
+    },
+    {
+    name: "New Genesis",
+    type: "Aim (Tournament)",
+    artist: "Ado",
+    difficulty: 6.46,
+    date: "19-09-2022",
+    bpm: 175,
+    link: "https://osu.ppy.sh/beatmapsets/1851189#osu/3802909"
+    },
+    {
+    name: "Bad for me (Dz'Xa's Amenpunk)",
+    type: "Experimental",
+    artist: "Official HIGE DANdism",
+    difficulty: 8.06,
+    date: "22-01-2023",
+    bpm: 200,
+    link: "https://osu.ppy.sh/beatmapsets/1927239#osu/3996526"
+    },
+    {
+    name: "Force of RA",
+    type: "Aim",
+    artist: "Memme",
+    difficulty: 7.03,
+    date: "25-09-2022",
+    bpm: 180,
+    link: "https://osu.ppy.sh/beatmapsets/1855106#osu/3812192"
+    },
+    {
+    name: "Ch3rry",
+    type: "Aim",
+    artist: "Katagiri",
+    difficulty: 8.27,
+    date: "15-02-2023",
+    bpm: 205,
+    link: "https://osu.ppy.sh/beatmapsets/1940811#osu/4013509"
+    },
+    {
+    name: "Black",
+    type: "AR8 Awkward Aim",
+    artist: "Toby Radiation Fox",
+    difficulty: 5.83,
+    date: "11-03-2023",
+    bpm: 165,
+    link: "https://osu.ppy.sh/beatmapsets/1954320#osu/4047416"
+    },
+    {
+    name: "Buntan Falling in B mix",
+    type: "Aim",
+    artist: "LMB",
+    difficulty: 8.23,
+    date: "11-03-2023",
+    bpm: 210,
+    link: "https://osu.ppy.sh/beatmapsets/1954337#osu/4047449"
+    },
+    {
+    name: "KUMAMIKO DANCING",
+    type: "Aim",
+    artist: "Hioka Natsumi & Yasumoto Hiroki feat Kumaide-mura no Minasan",
+    difficulty: 6.98,
+    date: "27-05-2023",
+    bpm: 165,
+    link: "https://osu.ppy.sh/beatmapsets/1999371#osu/4157512"
+    },
+    {
+    name: "Cat Loving",
+    type: "Aim",
+    artist: "Kotoha",
+    difficulty: 7,
+    date: "28-05-2023",
+    bpm: 163,
+    link: "https://osu.ppy.sh/beatmapsets/1999987#osu/4157778"
+    },
+    {
+    name: "Kigurumi Sungeki",
+    type: "Aim",
+    artist: "Chikamori Kayako",
+    difficulty: 7.73,
+    date: "05-12-2020",
+    bpm: 185,
+    link: "https://osu.ppy.sh/beatmapsets/1315615#osu/2726346"
+    },
+    {
+    name: "Nanairo Symphony",
+    type: "Aim",
+    artist: "Coala Mode.",
+    difficulty: 6.46,
+    date: "07-06-2023",
+    bpm: 155,
+    link: "https://osu.ppy.sh/beatmapsets/2005509#osu/4171056"
+    },
+   
+    // Add more map objects as needed
+];
+
+function populateTable() {
+  const tableBody = document.querySelector("#mapTableBody");
+  tableBody.innerHTML = ""; // Clear existing rows
+
+  maps.forEach((map) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${map.name}</td>
+      <td>${map.artist}</td>
+      <td>${map.type}</td>
+      <td>${map.date}</td>
+      <td>${map.difficulty.toFixed(2)}</td>
+      <td>${map.bpm}</td>
+      <td><a href="${map.link}" target="_blank">Link</a></td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+
+populateTable();
+
+// Store the current sort option and order
+let currentSortOption = "difficulty";
+let currentSortOrder = "asc";
+
+function sortTable(option) {
+  if (option === currentSortOption) {
+    currentSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
+  } else {
+    currentSortOption = option;
+    currentSortOrder = "asc";
+  }
+
+  maps.sort((a, b) => {
+    let aValue = a[currentSortOption];
+    let bValue = b[currentSortOption];
+
+    if (currentSortOption === "date") {
+      aValue = new Date(aValue.split("-").reverse().join("-"));
+      bValue = new Date(bValue.split("-").reverse().join("-"));
+    }
+
+    if (currentSortOption === "type") {
+      aValue = aValue.toLowerCase();
+      bValue = bValue.toLowerCase();
+    }
+
+    if (aValue < bValue) return currentSortOrder === "asc" ? -1 : 1;
+    if (aValue > bValue) return currentSortOrder === "asc" ? 1 : -1;
+    return 0;
+  });
+
+  populateTable();
+}
+
+const artistHeader = document.querySelector("#mapTable th:nth-child(2)");
+const bpmHeader = document.querySelector("#mapTable th:nth-child(6)");
+const dateHeader = document.querySelector("#mapTable th:nth-child(4)");
+const difficultyHeader = document.querySelector("#mapTable th:nth-child(5)");
+const typeHeader = document.querySelector("#mapTable th:nth-child(3)");
+
+artistHeader.addEventListener("click", () => sortTable("artist"));
+bpmHeader.addEventListener("click", () => sortTable("bpm"));
+dateHeader.addEventListener("click", () => sortTable("date"));
+difficultyHeader.addEventListener("click", () => sortTable("difficulty"));
+typeHeader.addEventListener("click", () => sortTable("type"));
